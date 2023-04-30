59e7fec7ecdd8410005495639257b5cdff80e0f1
iscc7 <30551381+iscc7@users.noreply.github.com>
2023-04-30 14:57:24 +0800

    updata plugins
    NA

--- init.lua
+++ init.lua
@@ -18,25 +18,26 @@ return {
         "mfussenegger/nvim-dap",
         enabled = true,
         config = function()
-          require("mason-nvim-dap").setup {
-            ensure_installed = { 'python' },
-            automatic_setup = true,
-            -- handlers = {
-            --   function(config)
-            --     require('mason-nvim-dap').default_setup(config)
-            --   end,
-            --   python = function(config)
-            --     config.adapter = {
-            --       type = "executable",
-            --       command = "C:/Users/y00800069/AppData/Local/Programs/Python/Python310/python.exe",
-            --       arg = {
-            --         "-m",
-            --         "debugpy.adapter",
-            --       },
-            --     }
-            --     require('mason-nvim-dap').default_setup(config)
-            --   end,
-            -- },
+          local dap = require('dap')
+          dap.adapters = {
+            python = {
+              type = "executable",
+              command = "python",
+              args = { "-m", "debugpy.adapter" },
+            },
+          }
+          dap.configurations = {
+            python = {
+              {
+                type = "python",
+                request = "launch",
+                name = "launch file",
+                program = "${file}",
+                pythonPath = function()
+                  return "python"
+                end
+              },
+            },
           }
         end,
       },
@@ -49,12 +50,38 @@ return {
         end,
         event = "VeryLazy",
       },
+      -- snippets
       {
         "L3MON4D3/LuaSnip",
         config = function(plugin, opts)
-          require "plugins.configs.luasnip" (plugin, opts)                                     -- include the default astronvim config that calls the setup call
+          require "plugins.configs.luasnip" (plugin, opts)                                       -- include the default astronvim config that calls the setup call
           require("luasnip.loaders.from_vscode").lazy_load { paths = { "./lua/user/snippets" } } -- load snippets paths
         end,
+      },
+      -- markdown-preview
+      {
+        "iamcco/markdown-preview.nvim",
+        ft = "markdown",
+        lazy = true,
+        build = "cd app && npm install && git reset --hard",
+        event = "VeryLazy",
+      },
+      -- code runner
+      {
+        "CRAG666/code_runner.nvim",
+        config = function()
+          require('code_runner').setup({
+            mode = "toggleterm",
+            -- put here the commands by filetype
+            filetype = {
+              python = "python -u",
+            },
+          })
+          -- code runner key mapping
+          local map = vim.api.nvim_set_keymap
+          map("n", "<leader>r", ":RunCode<CR>", { desc = "Run Code" })
+        end,
+        event = "VeryLazy",
       },
     },
   },
